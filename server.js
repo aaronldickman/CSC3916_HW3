47f6bc66844ec3144711be7cace250ffc2a6ea0b
Aaron Dickman <aaronldickman@gmail.com>
2021-03-13 16:32:48 -0700

    re-implemented api to better align with best practices

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ Description: Web API scaffolding for Movie API
 var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var authController = require('./auth');
 var authJwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
@@ -23,24 +22,6 @@ app.use(passport.initialize());
 
 var router = express.Router();
 
-function getJSONObjectForMovieRequirement(req) {
-    var json = {
-        headers: "No headers",
-        key: process.env.UNIQUE_KEY,
-        body: "No body"
-    };
-
-    if (req.body != null) {
-        json.body = req.body;
-    }
-
-    if (req.headers != null) {
-        json.headers = req.headers;
-    }
-
-    return json;
-}
-
 router.post('/signup', function(req, res) {
     if (!req.body.username || !req.body.password) {
         res.json({success: false, msg: 'Please include both username and password to signup.'})
@@ -52,7 +33,7 @@ router.post('/signup', function(req, res) {
 
         user.save(function(err){
             if (err) {
-                if (err.code == 11000){
+                if (err.code === 11000){
                     res.statusCode = 409;
                     return res.json({success: false, message: 'A user with that username already exists.'});
                 }
@@ -90,14 +71,18 @@ router.post('/signin', function (req, res) {
     })
 });
 
-router.route('/movies')
+router.route('/movies/:movieName?')
     .post(authJwtController.isAuthenticated, function(req, res){
-        if(!req.body.Title || !req.body.Year || !req.body.Genre || !req.body.LeadActors){
+        if(!req.params.movieName){
+            res.statusCode = 404;
+            return res.json({success: false, message: "Creating a movie must be done at path /movies/MovieName"})
+        }
+        if(!req.body.Year || !req.body.Genre || !req.body.LeadActors){
             res.statusCode = 400;
-            return res.json({success: false, message: "POST should have title, year, genre, and 3 lead actors."})
+            return res.json({success: false, message: "Creating a movie requires Year, Genre, and 3 LeadActors in body."})
         }
         let movie = new Movie();
-        movie.Title = req.body.Title;
+        movie.Title = req.params.movieName;
         movie.Year = req.body.Year;
         movie.Genre = req.body.Genre;
         movie.LeadActors = req.body.LeadActors;
@@ -119,31 +104,37 @@ router.route('/movies')
         })
     })
     .get(authJwtController.isAuthenticated, function(req, res){
-        if(!req.body.Title){
-            res.statusCode = 400;
-            return res.json({success: false, message: "GET should have Title attribute with title of desired movie in body."});
-        }
+        let limit = (req.query.limit &&
+                    req.query.limit > 0) ? parseInt(req.query.limit) : 10;
+        let offset = (req.query.offset &&
+                    req.query.offset >= 0) ? parseInt(req.query.offset) : 0;
+        let filters = extractFiltersFromRequest(req);
 
-        Movie.findOne({Title: req.body.Title}).select('Title Year Genre LeadActors').exec((err, movie) => {
-            if(err){
+        Movie.find(filters)
+            .skip(offset)
+            .limit(limit)
+            .select('Title Year Genre LeadActors')
+            .exec((err, movies) => {
+            if(err) {
                 res.statusCode = 500;
                 return res.json(err.message);
             }
-            else if(!movie){
-                res.statusCode = 404;
-                return res.json({success: false, message: "movie not found in database."})
+            else {
+                res.statusCode = 200;
+                return res.json({success: true, results: movies});
             }
-            else
-                return res.json({success: true, movie: movie});
-        });
+        })
     })
+
     .put(authJwtController.isAuthenticated, function(req, res) {
-        if(!req.body.Title || !req.body.FieldToUpdate || !req.body.NewValue){
+        if(!req.body.FieldToUpdate || !req.body.NewValue){
             res.statusCode = 400;
             return res.json({success: false, message: "PUT should have 'Title', 'FieldToUpdate', and 'NewValue' fields in body."});
         }
 
-        Movie.findOne({Title: req.body.Title}).select('Title Year Genre LeadActors').exec((err, movie) => {
+        let filters = extractFiltersFromRequest(req);
+
+        Movie.find(filters).select('Title Year Genre LeadActors').exec((err, movies) => {
             if(err){
                 if (err.code === 400){ //something was wrong with the provided data.
                     res.statusCode = 400;
@@ -155,57 +146,72 @@ router.route('/movies')
                     return res.json({success: false, message: "Internal server error."});
                 }
             }
-            if(movie === null){
+            console.log(movies);
+            if(movies.length === 0){
                 res.statusCode = 404;
-                return res.json({success: false, message: "A movie doesn't exist with that title."})
+                return res.json({success: false, message: "A movie doesn't exist with your parameters."})
             }
             else{
-                if(!movie[req.body.FieldToUpdate]){
+                if(!movies[0][req.body.FieldToUpdate]){
                     res.statusCode = 400;
                     return res.json({success: false, message: "Provided field to update does not exist."})
                 }
-                if(typeof req.body.NewValue !== typeof movie[req.body.FieldToUpdate]){
+                if(typeof req.body.NewValue !== typeof movies[0][req.body.FieldToUpdate]){
                     res.statusCode = 400;
                     return res.json({success: false, message: "Type of new value must match type of old value."})
                 }
-                movie[req.body.FieldToUpdate] = req.body.NewValue;
-                movie.save((err) =>{
-                    if(err){
-                        if(err.code === 400)
-                            res.statusCode = 400;
-                        else
-                            res.statusCode = 500;
-                        return res.json({success: false, message: err.message});
-                    }
-                    return res.json({success: true, message: "movie successfully updated"});
-                })
+                try {
+                    movies.forEach(element => {
+                        element[req.body.FieldToUpdate] = req.body.NewValue;
+                        element.save((err) => {
+                            if (err)
+                                throw(err)
+                        })
+                    })
+                    res.statusCode = 200;
+                    return res.json({success: true, message: "Values updated in database."});
+                }
+                catch(err){
+                    if(err.code === 400)
+                        res.statusCode = 400;
+                    else
+                        res.statusCode = 500;
+                    return res.json({success: false, error: err.message})
+                }
             }
 
         })
     })
     .delete(authJwtController.isAuthenticated, function(req, res){
-        if(!req.body.Title){
-            res.statusCode = 400;
-            return res.json({success: false, message: "DELETE should have Title attribute with title of desired movie in body." });
-        }
-        Movie.deleteOne({"Title": req.body.Title}).exec((err, result) => {
+        let filters = extractFiltersFromRequest(req);
+
+        Movie.deleteMany(filters).exec((err, result) => {
             if(err){
                     res.statusCode = 500;
                     return res.json({success: false, message: "Internal server error."});
                 }
             else if(result.n === 0){
                 res.statusCode = 404;
-                return res.json({success: false, message: "Movie couldn't be deleted, did not exist in database."});
+                return res.json({success: false, message: "Movie(s) couldn't be deleted, none matching your search exist in database."});
             }
             else {
                 res.statusCode = 200;
-                return res.json({success: true, message: "Movie deleted from database."});
+                return res.json({success: true, message: "Movie(s) deleted from database."});
             }
-
         })
     });
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
 module.exports = app; // for testing only
 
+function extractFiltersFromRequest(req){
+    let filters = {};
+    if(req.params.movieName)
+        filters.Title = req.params.movieName;
+    if(req.query.Year && req.query.Year > 0)
+        filters.Year = req.query.Year;
+    if(req.query.Genre)
+        filters.Genre = req.query.Genre;
 
+    return filters;
+}
